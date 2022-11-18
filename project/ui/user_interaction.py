6dc8558744a04958fdafd50cc664aa5b8dd48111
Michael Ptitsyn <mishapt@amazon.com>
2022-11-18 12:18:05 +0200

    assignment 3

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -7,7 +7,20 @@ the message that the user will see should be: <name>, please choose board size:
 the message that the user will see should be: <name>, for board size <board_size>, choose number of mines to allocate:
 4. print summery of all data
 """
-player_name = input("Hello, whats your name?")
-board_size = int(input(f"{player_name}, please choose board size:"))
-number_of_mines = int(input(f"{player_name}, for board size {board_size}, choose number of mines to allocate:"))
-print(f"{player_name}, the board size is: {board_size}, number of mines is: {number_of_mines}. ENJOY!")
+player_name, board_size, number_of_mines = None, None, None
+temp_player_name = input("Hello, whats your name?")
+if len(temp_player_name) > 1:
+    player_name = temp_player_name
+    temp_board_size = int(input(f"{player_name}, please choose board size:"))
+    if temp_board_size > 0 and temp_board_size < 26:
+        board_size = temp_board_size
+        temp_number_of_mines = int(
+            input(f"{player_name}, for board size {board_size}, choose number of mines to allocate:"))
+        if 0 < temp_number_of_mines <= 0.5 * board_size * board_size:
+            number_of_mines = temp_number_of_mines
+        else:
+            print(f"{player_name}, you entered illegal number of mines")
+    else:
+        print(f"{player_name}, you entered illegal board size")
+else:
+    print(f"Your name is too short")
