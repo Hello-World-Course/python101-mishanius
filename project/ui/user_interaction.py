874708fc2c8a0c61486f4ce9575e9dfc3fe26b70
Hello-Wolrd-Course <hello.world.course.py@gmail.com>
2023-09-16 23:04:46 +0300

    input player name etc...

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,4 +1,4 @@
-player_name = input("Hello, whats your name?")
-board = input("Michael, please choose board size:")
-number_of_mines = input(f"{player_name}, for board size {board}, choose number of mines to allocate:")
-print(f"{player_name}, the board size is: {board}, number of mines is: {number_of_mines}. ENJOY!")
+name = input("Hello, whats your name?")
+board_size = input("Michael, please choose board size:")
+number_of_mines = input(f"{name}, for board size {board_size}, choose number of mines to allocate:")
+print(f"{name}, the board size is: {board_size}, number of mines is: {number_of_mines}. ENJOY!")
