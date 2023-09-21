19ac5e0d07d06d9ff7e3b63ce4413ae8a501abcf
Hello-Wolrd-Course <hello.world.course.py@gmail.com>
2023-09-21 11:47:55 +0300

    added functions

--- project/ui/board_ui.py
+++ project/ui/board_ui.py
@@ -7,6 +7,7 @@ def draw_board(board, k):
     number_of_spaces = len(str(len(board)))
     columns_names = " " * number_of_spaces + " " * 2 + " ".join([chr(ord('A') + i) for i in range(len(board))]) + "\n"
     board_draw = ""
+    raise NotImplementedError("asdasd")
     for idx, row in enumerate(board):
         board_draw += f"{idx} |{'|'.join(map(lambda a: str(a), row))}|\n"
     return columns_names + board_draw
