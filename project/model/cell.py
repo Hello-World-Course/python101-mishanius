4cfedf4cf36cd06c0d9c108795b9a4ce17788b2d
Hello-Wolrd-Course <hello.world.course.py@gmail.com>
2023-09-21 14:30:46 +0300

    added functions

---
+++ project/model/cell.py
@@ -0,0 +1,25 @@
+class Cell:
+    def __init__(self, x, y) -> None:
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.clicked = False
+        self.flag = False
+
+    def set_clicked(self):
+        self.clicked = True
+
+    def is_clicked(self):
+        return self.clicked
+
+    def set_flag(self):
+        self.flag = True
+
+    def is_flaged(self):
+        return self.flag
+
+    def str_as_hidden(self):
+        return "F" if not self.is_flaged() else "_"
+
+    def str_as_clicked(self):
+        raise NotImplementedError()
