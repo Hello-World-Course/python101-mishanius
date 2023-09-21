5cef3a41937af5cbe8b478a7456525a3ae843955
Hello-Wolrd-Course <hello.world.course.py@gmail.com>
2023-09-21 12:43:02 +0300

    added functions

--- project/ui/board_ui.py
+++ project/ui/board_ui.py
@@ -5,14 +5,14 @@ def parse_cmd(command):
 
 def draw_board(board, k):
     number_of_spaces = len(str(len(board)))
-    columns_names = " " * number_of_spaces + " " * 2 + " ".join([chr(ord('A') + i) for i in range(len(board))]) + "\n"
+    columns_names = " " * number_of_spaces + " " * 2 + " ".join([chr(ord('A') + i) for i in range(len(board))]) + " \n"
     board_draw = ""
     for idx, row in enumerate(board):
         board_draw += f"{idx} |{'|'.join(map(lambda a: str(a), row))}|\n"
     return columns_names + board_draw
 
 
-def conver_coords(to_convert):
+def convert_coords(to_convert):
     row = to_convert[:-1]
     col = to_convert[-1]
     return int(row), ord(col) - ord("A")
