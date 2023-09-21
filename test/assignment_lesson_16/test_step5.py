ac717c3114c0b8d906d90add9de34a619145e0b0
Hello-Wolrd-Course <hello.world.course.py@gmail.com>
2023-09-21 15:04:08 +0300

    added functions

--- test/assignment_lesson_16/test_step5.py
+++ test/assignment_lesson_16/test_step5.py
@@ -32,79 +32,82 @@ class TestStep5(AssignmentTester):
         # test
         ######
         import project.model.mine as test_file
-        mine = test_file.Mine(0,0)
-        message.expectedResult = '*'
-        message.realResult = mine.str_as_clicked()
+        mine = test_file.Mine(0, 0)
+        expectedResult = '*'
+        realResult = mine.str_as_clicked()
         # verify
-        self.assertEqualWithMessage(message.expectedResult, message.realResult, msg=message)
+        self.assertEqualWithMessage(expectedResult, realResult, msg=message)
 
     @devin_test_decorator
     def test_empty_cell(self, message):
-        import project.model.empty_cell as test_file
-        empty_cell = test_file.EmptyCell(0,0)
-
-        message.expectedResult = ' '
-        message.realResult = empty_cell.str_as_clicked()
-        self.assertEqualWithMessage(message.expectedResult, message.realResult, msg=message)
-
-        empty_cell.val = 6
-        message.expectedResult = '6'
-        message.realResult = empty_cell.str_as_clicked()
-        self.assertEqualWithMessage(message.expectedResult, message.realResult, msg=message)
-
-    @devin_test_decorator
-    def test_empty_cell(self, message):
+        # test
+        ######
         import project.model.empty_cell as test_file
         empty_cell = test_file.EmptyCell(0, 0)
+        expectedResult = ' '
+        realResult = empty_cell.str_as_clicked()
+        # verify
+        self.assertEqualWithMessage(expectedResult, realResult, msg=message)
 
-        message.expectedResult = ' '
-        message.realResult = empty_cell.str_as_clicked()
-        self.assertEqualWithMessage(message.expectedResult, message.realResult, msg=message)
-
+    @devin_test_decorator
+    def test_empty_cell_second(self, message):
+        # test
+        ######
+        import project.model.empty_cell as test_file
+        empty_cell = test_file.EmptyCell(0, 0)
         empty_cell.val = 6
-        message.expectedResult = '6'
-        message.realResult = empty_cell.str_as_clicked()
-        self.assertEqualWithMessage(message.expectedResult, message.realResult, msg=message)
+        expectedResult = '6'
+        realResult = empty_cell.str_as_clicked()
+        # verify
+        self.assertEqualWithMessage(expectedResult, realResult, msg=message)
 
     @devin_test_decorator
     def test_board_length(self, message):
+        # test
+        ######
         import project.model.board as test_file
         b = test_file.Board(6)
 
-        message.expectedResult = 6
-        message.realResult = len(b)
-        message.explanation = {'value': 'CODE_MISMATCH'}
-        self.assertEqualWithMessage(message.expectedResult, message.realResult, msg=message)
+        expectedResult = 6
+        realResult = len(b)
+        # verify
+        self.assertEqualWithMessage(expectedResult, realResult, msg=message)
 
     @devin_test_decorator
     def test_board_get_item(self, message):
+        # test
+        ######
         import project.model.board as test_file
         import project.model.mine as mine_file
         board = test_file.Board(6)
-        mine = mine_file.Mine(1,1)
+        mine = mine_file.Mine(1, 1)
         board.board[1][1] = mine
 
-        message.expectedResult = mine
-        message.realResult = board[1][1]
-        message.explanation = {'value': 'CODE_MISMATCH'}
-        self.assertEqualWithMessage(message.expectedResult, message.realResult, msg=message)
+        expectedResult = mine
+        realResult = board[1][1]
+        # verify
+        self.assertEqualWithMessage(expectedResult, realResult, msg=message)
 
     @devin_test_decorator
     def test_board_set_flag(self, message):
+        # test
+        ######
         import project.model.board as test_file
         import project.model.mine as mine_file
         board = test_file.Board(6)
-        mine = mine_file.Mine(1,1)
+        mine = mine_file.Mine(1, 1)
         board.board[1][1] = mine
         board.set_flag(1, 1)
 
-        message.expectedResult = True
-        message.realResult = board[1][1].is_flag
-        message.explanation = {'value': 'CODE_MISMATCH'}
-        self.assertEqualWithMessage(message.expectedResult, message.realResult, msg=message)
+        expectedResult = True
+        realResult = board[1][1].is_flag
+        # verify
+        self.assertEqualWithMessage(expectedResult, realResult, msg=message)
 
     @devin_test_decorator
     def test_board_click(self, message):
+        # test
+        ######
         import project.model.board as test_file
         import project.model.mine as mine_file
         board = test_file.Board(6)
@@ -112,39 +115,55 @@ class TestStep5(AssignmentTester):
         board.board[1][1] = mine
         board.click(1, 1)
 
-        message.expectedResult = True
-        message.realResult = board[1][1].is_clicked
-        message.explanation = {'value': 'CODE_MISMATCH'}
-        self.assertEqualWithMessage(message.expectedResult, message.realResult, msg=message)
+        expectedResult = True
+        realResult = board[1][1].is_clicked
+        # verify
+        self.assertEqualWithMessage(expectedResult, realResult, msg=message)
 
-        message.expectedResult = False
-        message.realResult = board[0][0].is_clicked
-        message.explanation = {'value': 'CODE_MISMATCH'}
-        self.assertEqualWithMessage(message.expectedResult, message.realResult, msg=message)
+
+
+    @devin_test_decorator
+    def test_board_not_click(self, message):
+        # test
+        ######
+        import project.model.board as test_file
+        import project.model.mine as mine_file
+        board = test_file.Board(6)
+        mine = mine_file.Mine(1, 1)
+        board.board[1][1] = mine
+        board.click(1, 1)
+        expectedResult = False
+        realResult = board[0][0].is_clicked
+        # verify
+        self.assertEqualWithMessage(expectedResult, realResult, msg=message)
 
     @devin_test_decorator
     @mock.patch('sys.stdout', new_callable=io.StringIO)
     @mock.patch('builtins.input', side_effect=["f", "f", "f", "f", "Micael", "9", "40"])
     def test_terminal_init(self, mock_input, mock_stdout, message):
+        # test
+        ######
         import project.ui.terminal as test_file
         terminal = test_file.Terminal()
         terminal.init_game()
 
-        message.expectedResult = "Your name is too short\nYour name is too short\nYour name is too short\nYour name is too short\n"
-        message.realResult = mock_stdout.getvalue()
-        message.explanation = {'value': 'CODE_MISMATCH'}
-        self.assertEqualWithMessage(message.expectedResult, message.realResult, msg=message)
+        expectedResult = "Your name is too short\nYour name is too short\nYour name is too short\nYour name is too short\n"
+        realResult = mock_stdout.getvalue()
+        # verify
+        self.assertEqualWithMessage(expectedResult, realResult, msg=message)
 
     @devin_test_decorator
     @mock.patch('sys.stdout', new_callable=io.StringIO)
     @mock.patch('builtins.input', side_effect=["Micael", "9", "40"])
     def test_terminal_draw(self, mock_input, mock_stdout, message):
+        # test
+        ######
         import project.ui.terminal as test_file
         terminal = test_file.Terminal()
         terminal.init_game()
         terminal.draw()
 
-        message.expectedResult = EMPTY_BOARD
-        message.realResult = mock_stdout.getvalue()
-        message.explanation = {'value': 'CODE_MISMATCH'}
-        self.assertEqualWithMessage(message.expectedResult, message.realResult, msg=message)
+        expectedResult = EMPTY_BOARD
+        realResult = mock_stdout.getvalue()
+        # verify
+        self.assertEqualWithMessage(expectedResult, realResult, msg=message)
