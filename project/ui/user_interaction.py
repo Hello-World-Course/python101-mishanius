06326715f64a726f056358bc6353e8c4becabc3c
Hello-Wolrd-Course <hello.world.course.py@gmail.com>
2024-07-31 17:37:47 +0300

    Solved lesson_5

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,4 +1,5 @@
+# Hello
 name = input("Hello, whats your name")
 board_size = int(input(f"{name}, please choose board size"))
-number_of_mines = int(input(f"{name}, for {board_size}, choose number of mines to allocate:"))
+number_of_mines = int(input(f"{name}, for board size {board_size}, choose number of mines to allocate:"))
 print(f"{name}, the board size is: {board_size}, number of mines is: {7}. ENJOY!")
