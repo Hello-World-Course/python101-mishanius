c1639f1b3e6cd562d13b7dd824c19633811c492b
Hello-Wolrd-Course <hello.world.course.py@gmail.com>
2024-07-31 20:51:50 +0300

    added logic to get input from the user

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -2,24 +2,20 @@ name = None
 board_size = None
 number_of_mines = None
 
-input_name = input("Hello, what's your name: ")
-
-if len(input_name) <= 2:
+name_temp = input("Hello, what's your name: ")
+if len(name_temp) <= 2:
     print("Your name is too short")
 else:
-    name = input_name
-    board_size_input = input(f"{name}, please choose board size: ")
-
-    if not board_size_input.isdigit() or not (0 < int(board_size_input) < 26):
+    # We are ok
+    name = name_temp
+    board_size_temp = int(input(f"{name}, please choose board size: "))
+    if board_size_temp <= 0 or board_size_temp > 26:
         print(f"{name}, you have entered illegal board size")
     else:
-        board_size = int(board_size_input)
-        max_mines = (board_size * board_size) // 2
-
-        number_of_mines_input = input(f"{name}, for board size {board_size}, choose number of mines to allocate: ")
-
-        if not number_of_mines_input.isdigit() or not (0 < int(number_of_mines_input) <= max_mines):
+        board_size = board_size_temp
+        number_of_mines_temp = int(input(f"{name}, for board size {board_size}, choose number of mines to allocate"))
+        if number_of_mines_temp<= 0 or number_of_mines_temp > board_size*board_size//50:
             print(f"{name}, you have entered illegal number of mines")
         else:
-            number_of_mines = int(number_of_mines_input)
+            number_of_mines = number_of_mines_temp
             print(f"{name}, the board size is: {board_size}, number of mines is: {number_of_mines}. ENJOY!")
