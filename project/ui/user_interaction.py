64e57fc53dbf3a876107d9a191fd2cafd5604d6c
Hello-Wolrd-Course <hello.world.course.py@gmail.com>
2024-11-19 01:21:31 +0200

    2

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,4 +1,5 @@
 # THIS CODE IS WRONG, FIX IT AND ADD NEW CODE
-name = input("HeLo WAT Name you")
-b_size = int(input(f"{name}, please choose b size"))
-print(f"{name}, the board size is: {b_size}, number of mines is: anfkja")
+name = input("Hello, whats your name	")
+board_size = int(input(f"{name}, please choose board size"))
+number_of_mines = int(input(f"{name}, for board size {board_size}, choose number of mines to allocate"))
+print(f"{name}, the board size is: {board_size}, number of mines is: {number_of_mines}. ENJOY")
